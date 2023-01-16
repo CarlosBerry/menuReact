655baab66d6e570837479efea1d089fce9b889cf
CarlosBerry <99772893+CarlosBerry@users.noreply.github.com>
2023-01-16 08:26:48 -0500

    commit to remote repository

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello menu React App!</Text>
+      <Text>Hello menu React App and repository in github!</Text>
       <StatusBar style="auto" />
     </View>
   );
