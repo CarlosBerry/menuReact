5ce548bc64465867cd778ffe1d4820355d184de4
CarlosBerry <99772893+CarlosBerry@users.noreply.github.com>
2023-01-18 15:58:08 -0500

    adding images and styling to mealitem

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,12 +1,59 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 
-function MealItem({title}){
+function MealItem({title, imageUrl, duration, complexity, affordability}){
     return (
-        <View>
-            <Text>{title}</Text>
+        <View style={styles.mealItem}>
+         <Pressable android_ripple={{color:'#ccc'}}>
+            <View>
+            <View>
+                <Image source={{uri: imageUrl}} style={styles.image}/>
+            </View>
+            <Text style={styles.title}>{title}</Text>
+            <View style={styles.details}>
+                <Text style={styles.detailItem}>{duration}-m</Text>
+                <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+                <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+            </View>
+            </View>
+         </Pressable>
         </View>
     )
 
 };
 
 export default MealItem;
+
+const styles= StyleSheet.create({
+    image:{
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        textAlign: 'center',
+        margin: 8
+    },
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        elevation: 4
+    },
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 8,
+        justifyContent: 'center'
+    },
+    detailItem:{
+        marginHorizontal: 6,
+        fontSize: 12
+    },
+    innerContainer:{
+        borderRadius: 8,
+        overflow: 'hidden'
+    }
+
+});
